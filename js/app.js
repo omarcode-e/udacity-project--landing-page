1db022d278e1e87eecf180b2a66f155d51ac091e
Omar-Code-Coding <omaryasser_250@hotmail.com>
2022-10-22 17:37:58 +0200

    refactor: create new helper functions

--- js/app.js
+++ js/app.js
@@ -25,36 +25,54 @@
  */
 const navbarList = document.querySelector("#navbar__nav");
 const sections = document.querySelectorAll("[data-nav]");
-
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
+/**
+ * Create and insert a new Element into inputed parent-Element with optional
+ * arguments such as textContent and attributes to set to new Element.
+ *
+ * @param {string} tag - A String specifying the type of tag to be created.
+ * @param {Element} parentElement - An HTML-element for appending created Element into.
+ * @param {string} textContent - A String to be assigned to created Element.
+ * @param {object} attributes - An Object of attribute-name, value to set for created Element.
+ * @returns Newly appended element
+ */
+// prettier-ignore
+function renderElement(tag, parentElement, textContent = "", attributes = {}) {
+  const newElement = document.createElement(tag);
+  if (textContent) {
+    newElement.textContent = textContent;
+  }
+  
+  if(attributes) {
+    Object.entries(attributes).forEach(([attrName, attrValue]) =>
+      newElement.setAttribute(attrName, attrValue)
+    );
+  }
+  parentElement.append(newElement);
+  return newElement;
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
  *
  */
-function app() {
-  initNav(navbarList, sections);
-  setNavHandler(navbarList);
-}
+
 // build the nav
 function renderNavItem(parentEl, navLinktext, navLinkAttrValue) {
-  const navItem = document.createElement("li");
-  const navLink = document.createElement("a");
-  parentEl.append(navItem);
-  navItem.append(navLink);
-  navLink.setAttribute("href", navLinkAttrValue);
-  navLink.classList.add("nav__link");
-  return (navLink.textContent = navLinktext);
+  const navItem = renderElement("li", parentEl);
+  const navLink = renderElement("a", navItem, navLinktext, {
+    class: "nav__link",
+    href: navLinkAttrValue,
+  });
+  return navLink;
 }
 
 function initNav(parentEl, sections) {
-  if (!sections) return;
-
   return sections.forEach((section) => {
     const sectionHeading = section.getAttribute("data-nav");
     const sectionId = `#${section.id}`;
@@ -64,32 +82,29 @@ function initNav(parentEl, sections) {
 
 function setNavHandler(parentEl, handler) {
   return parentEl.addEventListener("click", (e) => {
-    if (e.target.className !== "nav__link") return;
-    e.preventDefault();
-    console.log(e.target.hash);
-    const currentSection = document.querySelector(e.target.hash);
-    currentSection.scrollIntoView({ behavior: "smooth", block: "end" });
-    console.log("nav-item clicked");
-    handler;
+    handler(e);
   });
 }
-
-function scrollToSection(e) {}
-
-app();
 
 // Add class 'active' to section when near top of viewport
 
 // Scroll to anchor ID using scrollTO event
+function scrollToSection(e) {
+  if (e.target.className !== "nav__link") return;
+  e.preventDefault();
+  const currentSection = document.querySelector(e.target.hash);
+  return currentSection.scrollIntoView({ behavior: "smooth", block: "center" });
+}
 
 /**
  * End Main Functions
  * Begin Events
  *
  */
+initNav(navbarList, sections);
 
 // Build menu
-
 // Scroll to section on link click
+setNavHandler(navbarList, scrollToSection);
 
 // Set sections as active
